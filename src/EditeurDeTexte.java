10f814b319d9c93e2173b8c3f908d93c8de9ac87
Eluriel <samuel.sicard.it@gmail.com>
2016-05-14 18:57:06 +0200

    #création de la classe main

---
+++ src/EditeurDeTexte.java
@@ -0,0 +1,13 @@
+
+public class EditeurDeTexte {
+
+	/**
+	 * Classe principale de l'application "Editeur De Texte"
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
