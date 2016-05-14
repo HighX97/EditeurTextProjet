d98cc0cb90b45a2c22acf950e8423dc443e6d418
Eluriel <samuel.sicard.it@gmail.com>
2016-05-14 18:51:47 +0200

    #essai

--- src/L_Classe.java
+++ src/L_Classe.java
@@ -1,11 +1,5 @@
 
-public class L_Classe 
-{
-	String text;
-	
-	L_Classe()
-	{
-		text = "Ready to work";
-	}
+public class L_Classe {
 
+	
 }
---
+++ src/STest.java
@@ -0,0 +1,9 @@
+
+public class STest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("J'espère que ça va marcher !");
+	}
+
+}
